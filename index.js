136336c992f45941386f4bc8497d81084ec36837
rifthdyt <37908851+rifthdyt@users.noreply.github.com>
2018-05-06 19:45:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -81,4 +81,20 @@ bot.on("message", async message => {
 
 });
 
+bot.on("message", async message => {
+
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+
+    let prefix = '-';
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (cmd === `${prefix}twitch`) {
+        message.channel.send("https://www.twitch.tv/officialnoted");
+    }
+
+});
+
 bot.login(process.env.token);
