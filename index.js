70ce9b2341b99ba3ad64804a15f1ac1c87fca5ba
rifthdyt <37908851+rifthdyt@users.noreply.github.com>
2018-09-09 19:10:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -108,6 +108,23 @@ bot.on("message", async message => {
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
 
+    let prefix = '?';
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (cmd === `${prefix}catpics`) {
+        message.channel.send("random.cat");
+    }
+
+});
+
+
+bot.on("message", async message => {
+
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+
     let prefix = '-';
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
